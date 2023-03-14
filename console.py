84c99a1cd7b5a8cedcddbde8879cf1dc95707fe9
Abraham Afework <Abrahamafework7@gmail.com>
2023-03-14 09:54:23 +0200

    console.py added

--- console.py
+++ console.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
 import cmd
-""" class for command """
-
+"""
+The console v: 0.0.1
+Contains the entry point of the command interpreter
+"""
 class HBNBCommand(cmd.Cmd):
-
-    """ some document """
+    """
+    HBNBCommand class is a command-line interface for some program.
+    """
 
     prompt = "(hbnb) "
-    
+
     def do_quit(self, arg):
-        """ Quit command to exit the program """
+        """
+        Quit command to exit the program
+        Usage: quit
+        """
         exit(1)
 
     def do_EOF(self, arg):
-       """ CTRL-D to exit the program """
+       """
+       EOF command to exit the program
+       Usage: CTRL-D
+       """
        exit(1)
-       print()
+
     def emptyline(self):
+        """
+        Do nothing on empty input line
+        """
         pass
 
 if __name__ == '__main__':
