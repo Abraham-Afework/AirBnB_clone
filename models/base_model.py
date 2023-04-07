849253c5b0e9c7496d67ff48cfe63b43d9a7cae8
Abraham Afework <Abrahamafework7@gmail.com>
2023-04-07 14:32:41 +0200

    BaseModel amended

--- models/base_model.py
+++ models/base_model.py
@@ -19,15 +19,14 @@ class BaseModel():
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    setattr(self, key, value)
+        
+        for key, value in kwargs.items():
+            if key == '__class__':
+                  continue
+            if key == 'created_at' or key == 'updated_at':
+               value = datetime.datetime.strptime(
+                      value, "%Y-%m-%dT%H:%M:%S.%f")
+            setattr(self, key, value)
 
     def __str__(self):
         """
